240566f367cfb1a625843b8bd6fffc54dd79964a
khaled-alaa <khalid.m.alaa@gmail.com>
2020-11-30 15:41:11 +0200

    feature : add about info component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import MainBar from "./Components/mainbar/mainbar";
 import Slider from "./Components/slider/slider";
 import FooterBar from "./Components/footer/footer";
+import AboutInfo from "./Components/about/about";
 /*import Card from "./Components/card";
 import Form from "./Components/form";*/
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -14,6 +15,7 @@ function App() {
       <div className="App">
         <MainBar />
         <Route exact path="/" render={() => <Slider />} />
+        <Route path="/creator" component={AboutInfo} />
         {/*}<Route path="/shop" component={Card} />
         <Route path="/contactus" component={Form} />{*/}
         <FooterBar />
