70baf84542dd0c41f7dddb320d370bb0c3c43bb0
khaled-alaa <khalid.m.alaa@gmail.com>
2020-12-07 15:44:26 +0200

    create data file by json-server

---
+++ json-server-lib/data.js
@@ -0,0 +1,62 @@
+module.exports = () => {
+  const companyData = {
+    products: [
+      {
+        id: "p1",
+        product_name: "Divol",
+        product_img:
+          "https://apollo-ireland.akamaized.net/v1/files/7c6qfaneulsn1-EG/image;s=644x461;olx-st/_2_.jpg",
+        product_pref: "Divol is a brand for kids natural shampoo 300ml",
+      },
+      {
+        id: "p2",
+        product_name: "penduline",
+        product_img:
+          "https://eg.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/34/325511/1.jpg?6780",
+        product_pref: "penduline is a brand for kids natural shampoo 500ml",
+      },
+      {
+        id: "p3",
+        product_name: "head & shoulders",
+        product_img:
+          "https://res.cloudinary.com/mtree/image/upload/q_auto,f_auto/HeadandShoulders_PH_MW/9Gq7gblVJdM5RfPkfdp5H/6302bb00431710a9b9abf450a31b73e3/HS_PH_Menthol_Large.jpg",
+        product_pref: "head & shoulders is a shampoo 490ml",
+      },
+      {
+        id: "p4",
+        product_name: "nivea",
+        product_img:
+          "https://images-na.ssl-images-amazon.com/images/I/61bHRocuBzL._SL1400_.jpg",
+        product_pref: "nivea 300ml",
+      },
+      {
+        id: "p5",
+        product_name: "dove",
+        product_img:
+          "https://images-na.ssl-images-amazon.com/images/I/71tbL2-6hvL._SL1500_.jpg",
+        product_pref: "dove 330ml",
+      },
+    ],
+    advertising: [
+      {
+        id: "a1",
+        ad_img:
+          "https://cms.souqcdn.com/cms/boxes/img/desktop/S_1475685111_mobiles-en.jpg",
+        ad_link: "rob23@gmail.com",
+      },
+      {
+        id: "a2",
+        ad_img:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkmxpc5AeWtWoWqRmvqtbZCjtpt9QI4xa3OQ&usqp=CAU",
+        ad_link: "rob23@gmail.com",
+      },
+      {
+        id: "a3",
+        ad_img:
+          ".https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvqJdqQxUik-pmxlzpT11saNFdhO5o_4HYkA&usqp=CAU",
+        ad_link: "rob23@gmail.com",
+      },
+    ],
+  };
+  return companyData;
+};
